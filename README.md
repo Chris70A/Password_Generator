a0b914c7e9a6d6018afb65407d8246bfb86a5210
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2022-12-22 15:44:59 -0800

    Fixed issue with password length loop and user input length

--- README.md
+++ README.md
@@ -1,65 +1,78 @@
 # Password Generator
 
 ## Description
-In this weeks challenge I created 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+In this weeks challenge I created an application that an employee can use to generate a random password based on criteria they've selected. How many characters would your password contain? If you want your password to have any numbers ,lowerCase, Uppercase, and Special characters.
+![char code chart](/assets/functions.png )
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+- One of the primary motivations for creating a password generator is to improve the security of login passwords. By generating random, complex passwords, it becomes much more difficult for hackers to crack your password.
+- Creating strong passwords can be difficult, especially if you need to come up with multiple unique passwords for different accounts. A password generator can make the process of creating strong passwords much easier by generating random passwords that meet certain criteria
+-  It can be difficult for people to come up with complex passwords that are hard to guess or crack. A password generator can generate random, complex passwords that are much more secure than ones that are easy to guess.
+- I learned how to create and manipulate strings in JavaScript using string methods like concat(), slice(), and substring(). using for loops to repeat certain actions and generate a certain number of characters, as well as using conditional statements to determine which characters or numbers to include in the password based on certain criteria. I learned Functions can be very useful for organizing and reusing code, as they allow you to define a set of instructions that can be called multiple times from different parts of your program. Ive also learned about JavaScript String.fromCharCode() this method is used to create a string from the given sequence of UTF-16 code units.
+![char code chart](/assets/charCodes.png )
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
+## Table of Contents 
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+- [Features](#Features)
 
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
-
-![alt text](assets/images/screenshot.png)
+![features of the app](/assets/features.gif )
+A password generator can help you meet security requirements. Some websites or services may have specific requirements for passwords, such as a minimum length or the use of certain characters. A password generator can help you create a password that meets these requirements.
+- How many characters would your password contain?
+- Special characters
+- number characters
+- lowerCase characters
+- UpperCase characters
 
 ## Credits
+It's important to be gracious and recognize the contributions of others, as it helps to build a sense of community and fosters a positive and collaborative environment. 
 
-List your collaborators, if any, with links to their GitHub profiles.
+Id like to give credit to this genus Ricardo Cisneros for working on this project with me. 
+- https://www.linkedin.com/in/ricardo-cisneros-5b8b3024b/ 
+- https://github.com/ricardoci
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
-If you followed tutorials, include links to those here as well.
+Id like to give credit to this video helping me understanding and writing cleaner code for the password generator. 
+- https://youtu.be/iKo9pDKKHnc 
+
+
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+MIT License
 
----
+Copyright (c) [2022] [Christian Plasencia]
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 
 ## Badges
 
 ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
 
-Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 ## Features
 
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+- Length of password 8 - 129 characters
+- Special characters
+- number characters
+- lowerCase characters
+- UpperCase characters
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,23 @@
 // Assignment code here
 
 
-//Here we are using the function arrayFromLowToHigh to grab the chars that we want and storing them in a variable in the global scope.
-var UpperCase = arrayFromLowToHigh(65, 90)
-var LowerCase = arrayFromLowToHigh(97, 122)
-var NumberChar = arrayFromLowToHigh(48, 57)
-var SymboChar = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+//Here we are using the function arrayFromLowToHigh to store the chars that we want in a variable in the global scope.
 
+var UpperCase = arrayFromLowToHigh(65, 90) // Characters(65 - 90)= A-Z Uppercase 
+var LowerCase = arrayFromLowToHigh(97, 122) // Characters(97 - 122)= a-z Lowercase 
+var NumberChar = arrayFromLowToHigh(48, 57) // Characters(48 - 57)= 0-9 Numbers
+var SymboChar = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126)) // Characters (33-47)(58-64)(91-96)(123-126) symbols
 
-var s = String.fromCharCode.apply(null, SymboChar);
-var l = String.fromCharCode.apply(null, LowerCase);
-var u = String.fromCharCode.apply(null, UpperCase);
-var n = String.fromCharCode.apply(null, NumberChar);
-
+var s = String.fromCharCode.apply(null, SymboChar); // Characters (33-47)(58-64)(91-96)(123-126) symbols
+var l = String.fromCharCode.apply(null, LowerCase); // Characters(97 - 122)= a-z Lowercase 
+var u = String.fromCharCode.apply(null, UpperCase); // Characters(65 - 90)= A-Z Uppercase 
+var n = String.fromCharCode.apply(null, NumberChar); // Characters(48 - 57)= 0-9 Numbers
+console.log(typeof s)
 // split method converts string into array
 s = s.split('');
 l = l.split('');
-u = u.split('');
 n = n.split('');
-
-
+u = u.split('');
 
 
 // This function loop, sorts the chars from low to high; We don't use methods because we are fecthing these from the char method.
@@ -32,74 +30,138 @@ function arrayFromLowToHigh(low, high) {
   }
   return array
 }
+// end of our low to hight function
 
-// fuction that gives the user with the password rules
-function generatePassword() {
-  //empty array that will store the prompt values
-  var vaule = [];
 
-  //if statment that will alert user if they don't enter 8 or more charcters
-  value = prompt("How many characters would your password contain? ");
-  if(value < 8) {
-    alert('character must be more than 8 character!');
-  }
-// if statment alerting user the length must be less then 129 characters
-  if (value > 129){
-  alert('Password length must be less than 129 characters');
-  return null;
-  }
 
-var trueChar = {
-   sChar: true,
-   nchar: true,
-   uChar: true,
-   nChar: true,
+
+// function that provides user with the password rules
+function passwordRules(){
+  
+
+  // The parseInt method turns the string into a interger which allows for us to use it in a if statment and in our password length loop.
+   var value = parseInt(prompt("How many characters would your password contain?"),10);
+
+  
+
+   
+    // if statment using the or operators alerting user if they don't enter the correct range 8-129 
+    if( value < 8 || value > 129 || !value ){
+    alert('password length should be a number, Character must be more than 8 characters and must be less than 129 characters')
+    // return null so that our prompt exits
+    return null
+   }
+   
+   // confrim adds a OK/True or CANCEL/false button to our prompt. Here we add it to know what characters our user wants
+   var sChar = confirm('click to add Special characters');
+   var nChar = confirm('click to add number characters');
+   var lChar = confirm('click to add lowerCase characters');
+   var uChar = confirm('click to add UpperCase characters');
+  
+   // if statment that return an alert to the user if they don't make a characters selection. if the user hits the CANCEL/false button that is displayed by the confirm method, this if statment gets executed.
+   if (!sChar && !nChar && !lChar && !uChar ){
+    
+    alert('Must select at least one character type')
+    // return null so that our prompt exits
+    return null
+   
+   }
+   // We stored our password in an object without it we won't get the value displayed.
+   var passwordStorage = {
+    value: value,
+    sChar: sChar,
+    nChar: nChar,
+    uChar: uChar,
+    lChar: lChar,
+   };
+   return passwordStorage
+  
 }
+//End of password rules function 
+
+// Function will create a random char by pulling chars from the chosen arrays below.
+function randomPassword(arr){
+  
+  var rp = Math.floor(Math.random() * arr.length);
+  var pr = arr[rp];
+
+   return pr;
+}
+
+
+
+function generatePassword(){
+
+  // storing the function passwordRules in a local variable to have access to it's data.
+  var options = passwordRules();
+
+  // the code starts by creating empty arrays 
+var lastPassword = [];
+
+var secondInteration = [];
+
+var fisrtIteration = [];
+
+// if option is false it will return null
+if (!options) return null;
+
+
+
+// each of these if statment will pull one character from each char.
+if (options.sChar) {
+  secondInteration = secondInteration.concat(s);
+  fisrtIteration.push(randomPassword(s));
+
+ 
+}
+if (options.lChar) {
+  secondInteration = secondInteration.concat(l);
+  fisrtIteration.push(randomPassword(l));
+}
+if (options.uChar) {
+  secondInteration = secondInteration.concat(u);
+  fisrtIteration.push(randomPassword(u));
+}
+
+if (options.nChar) {
+  secondInteration = secondInteration.concat(n);
+  fisrtIteration.push(randomPassword(n));
+}
+
+
  
 
 
+// this for loop will use the users input for the length of the password.
+for(var i = 0; i < options.value; i++) {
+  var possibleCharacter = randomPassword(secondInteration);
 
- var falseChars = {
-    sChar: false,
-    nChar: false,
-    uChar: false,
-    nChar: false,
-  };
-    
-
-
-if (falseChars) {
-
-  alert('Must select at least one character type');
-
+  lastPassword.push(possibleCharacter);
 }
+for (var i = 0; i < fisrtIteration.length; i++) {
+  lastPassword[i] = fisrtIteration[i];
+}
+//Finally, all of these passwords are combined together into one string using join().
+return lastPassword.join('');
 }
 
 
-function randomPassword() {
-  
-  var rp = math.floor(Math.random) * a.length;
-  var pr = a[rp];
-  return pr
-}
 
-function generatePassword{
-  
-}
+
+
 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
